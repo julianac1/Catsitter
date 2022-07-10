8f311733cddf056f2c5d585e86b2e668a3876f5d
juliana <julianacmenezes@msn.com>
2022-07-10 16:59:52 -0400

    primera entrega

---
+++ JavaScript/script.js
@@ -0,0 +1,77 @@
+
+
+//mensage de bienvenida - pagina inicial
+
+let bienvenida = prompt('Buscas una catsitter para tu michi? Sí o No');
+
+function inicio (respuestaA, respuestaB){
+    let entrar = "Sí";
+    let salir = "No";
+    if (bienvenida == entrar){
+        alert ('Bienvenida(o), cuidaremos muy bien de tu michi!');
+    } else if (bienvenida == salir){
+        alert ("Acá amamos a los michis. Si no te gusta los gatos, te invitamos a visitar otras páginas");
+    } else {
+        alert ("ERROR. Debes escribir Sí o No");
+        prompt('Buscas una catsitter para tu michi? Sí o No');
+    }
+    
+};
+
+inicio();
+
+//mensaje de bienvenida sin function
+/*let bienvenida = prompt('Buscas una catsitter para tu michi? Sí o No');
+let entrar = "Sí";
+let salir = "No";
+
+if (bienvenida == entrar){
+    alert ('Bienvenida(o), cuidaremos muy bien de tu michi!');
+} else if (bienvenida == salir){
+    alert ("Acá amamos a los michis. Si no te gusta los gatos, te invitamos a visitar otras páginas");
+} else {
+    alert ("ERROR. Debes escribir Sí o No");
+    prompt('Buscas una catsitter para tu michi? Sí o No');
+}*/
+
+
+
+
+
+//Bootstrap cards con fotos y comentarios de clientes - secion clientes.html
+
+let cards = [{
+    image: 'https://estaticos.muyinteresante.es/media/cache/1000x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-caja_0.jpg',
+    title: 'Alba y Astra',
+    descripcion: 'Por definir',
+},
+{
+    image: 'https://estaticos.muyinteresante.es/media/cache/1000x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-frotandose-piernas.jpg',
+    title: 'Emily',
+    descripcion: 'Por definir',
+},
+{
+    image: 'https://estaticos.muyinteresante.es/media/cache/1000x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-persa-bandeja.jpg',
+    title: 'Milagros',
+    descripcion: 'Por definir',
+},
+{
+    image: 'https://estaticos.muyinteresante.es/media/cache/1000x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-amasando.jpg',
+    title: 'Flamy',
+    descripcion: 'Por definir',
+},
+];
+
+for (let i = 0; i < cards.length; i++){
+    clientes.innerHTML += `
+    <div class="card" style="width: 18rem;">
+    <img src="${cards[i].image}" class="card-img-top" alt="Astra y Alba">
+    <div class="card-body">
+    <h5 class="card-title">${cards[i].title}</h5>
+    <p class="card-text">${cards[i].descripcion}</p>
+    </div>
+    </div>
+    `
+}
+
+
